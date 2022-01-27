8957740b904379479bedeae77b1dd568418ef819
keteague <kteague+github.com@pobox.com>
2022-01-26 18:20:03 -0600

    Add files via upload

---
+++ ADAccountPWAudit.ps1
@@ -0,0 +1,7 @@
+﻿$groupMembers = (Get-ADGroupMember "Domain Admins").SamAccountName
+
+$output = ForEach ($groupMember in $groupMembers) {
+    Get-ADUser -Identity $groupMember -Properties Created,LastLogonDate,PasswordLastSet | Select-Object -Property Name, SAMAccountName, Created, LastLogonDate, PasswordLastSet
+}
+
+$output | Export-Csv 'C:\temp\GroupMembers.csv'
